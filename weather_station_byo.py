2ca0f5479eb0684953040d796b7c3a8515276db8
theoracle09 <josh.daupert@gmail.com>
2021-07-09 18:27:11 -0700

    Send all sensor values as json object via mqtt.

--- weather_station_byo.py
+++ weather_station_byo.py
@@ -6,6 +6,8 @@ import bme280_sensor
 import wind_direction_byo
 import ds18b20_therm
 import database
+import paho.mqtt.client as mqtt
+import json
 
 # Constant variable definition
 CM_IN_A_KM = 100000.0
@@ -25,6 +27,14 @@ radius_cm = 9.0          # Radius of anemometer
 wind_interval = 5        # How many secs to collect wind dir and speed
 interval = 5             # Data collection interval in secs. 5 mins = 5 * 60 = 300
 rain_count = 0           # Counts rain bucket tips
+
+#Connect to MQTT
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("192.168.1.16", 1883, 60)
 
 # Every half-rotation, add 1 to count
 def spin():
@@ -92,16 +102,34 @@ while True:
     
     humidity, pressure, ambient_temp = bme280_sensor.read_all()
 
-    # Round wind_direction, humidity, pressure, ambient_temp, and ground_temp to 1 decimals 
+    # Round wind_direction, humidity, pressure, ambient_temp, ground_temp, and rainfall to 1 decimals 
     # and convert C readings to F
     wind_direction = round(wind_direction)
     humidity = round(humidity, 1)
     pressure = round(pressure, 1)
     ambient_temp = celsius_to_f(round(ambient_temp, 1))
     ground_temp = celsius_to_f(round(ground_temp, 1))
-    
+    rainfall = round(rainfall, 1)
     
     print(wind_speed, wind_gust, rainfall, wind_direction, humidity, pressure, ambient_temp, ground_temp)
+
+    # Create JSON object for MQTT transmission
+    send_msg = {
+        'wind_speed': wind_speed,
+        'wind_gust': wind_gust,
+        'rainfall': rainfall,
+        'wind_direction': wind_direction,
+        'humidity': humidity,
+        'pressure': pressure,
+        'ambient_temp': ambient_temp,
+        'ground_temp': ground_temp
+    }
+
+    # Convert message to json
+    payload = json.dumps(send_msg)
+
+    # Publish to mqtt
+    client.publish("raspberry/ws/sensors", payload, qos=1)
 
     # Record to database
     db.insert(ambient_temp, ground_temp, 0, pressure, humidity, wind_direction, wind_speed, wind_gust, rainfall)
